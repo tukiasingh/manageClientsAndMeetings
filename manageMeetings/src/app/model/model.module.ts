106f46228382dbe3149dd5d556d483663bb9b962
tukiasingh <vijayprakashsingh0323@gmail.com>
2024-03-23 23:41:47 -0500

    add model module

---
+++ manageMeetings/src/app/model/model.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from "@angular/core";
+import {DataSource} from "../static.datasource";
+import {Model} from "../repository.model";
+
+@NgModule({
+  providers: [Model, DataSource]
+})
+
+export class ModelModule {}
